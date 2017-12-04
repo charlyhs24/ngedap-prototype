0e45c1b121dfea26a43ab560e9d4232bf6018bf2
kukuh-sulistyo <sulistyo.kukuh@gmail.com>
2017-12-04 20:31:51 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Stylesheets
+import "../node_modules/uswds/dist/css/uswds.min.css";
+import "./stylesheets/main.scss";
+
+// Javascript
+import './js/main.js'
+
+var uswds = require('uswds');
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.$ = jQuery;
+// Enable HotModuleReplacment
+if (module.hot) module.hot.accept();
